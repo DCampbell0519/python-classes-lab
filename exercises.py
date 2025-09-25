84e6b58435adf7aa15179d3bb7ce8ebfc29e3bd9
Donovan Campbell <donovan@dlcentertainment.net>
2025-09-24 20:16:46 -0700

    handle player input

---
+++ exercises.py
@@ -0,0 +1,50 @@
+class Game():
+    def __init__(self):
+        self.turn = 'X'
+        self.tie = False
+        self.winner = None
+        self.board = {
+            'a1': None, 'b1': None, 'c1': None,
+            'a2': None, 'b2': None, 'c2': None,
+            'a3': None, 'b3': None, 'c3': None,
+        }
+    
+    def play_game(self):
+        print('I love me some tic-tac-toe!')
+
+    def print_board(self):
+        board = self.board
+        print(f"""
+                A   B   C
+            1)  {board['a1'] or ' '} | {board['b1'] or ' '} | {board['c1'] or ' '}
+                ----------
+            2)  {board['a2'] or ' '} | {board['b2'] or ' '} | {board['c2'] or ' '}
+                ----------
+            3)  {board['a3'] or ' '} | {board['b3'] or ' '} | {board['c3'] or ' '}
+        """)
+    
+    def print_message(self):
+        if self.tie:
+            print('Tie Game!')
+        elif self.winner:
+            print(f'{self.winner} wins the game!')
+        else: 
+            print(f'It\'s player {self.turn}\'s turn!')
+
+    def render(self):
+        Game.play_game(self)
+        Game.print_board(self)
+        Game.print_message(self)
+    
+    def place_piece(self):
+        
+
+tic_tac_toe = Game()
+# tic_tac_toe.play_game()
+# tic_tac_toe.print_board()
+# tic_tac_toe.winner = 'X'
+# tic_tac_toe.tie = True
+# print(tic_tac_toe.winner)
+# print(tic_tac_toe.tie)
+# tic_tac_toe.print_message()
+tic_tac_toe.render()
